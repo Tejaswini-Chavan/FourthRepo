f14a7149e0023171c504966e837a5c3acf9d6e35
Tejaswini <oktapoc11@gmail.com>
2022-07-20 18:07:32 +0530

    getter change

--- XmartApp/src/main/java/com/demo/xmartapp/entity/CustomerEntity.java
+++ XmartApp/src/main/java/com/demo/xmartapp/entity/CustomerEntity.java
@@ -41,9 +41,10 @@ public class CustomerEntity {
     public void setEmailId(String emailId) {
         this.emailId = emailId;
     }
-    public String getName() {
-        return name;
-    }
+
+	/*
+	 * public String getName() { return name; }
+	 */
     public void setName(String name) {
         this.name = name;
     }
@@ -54,16 +55,17 @@ public class CustomerEntity {
     public void setPassword(String password) {
         this.password = password;
     }
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+
+	/*
+	 * public String getPhoneNumber() { return phoneNumber; }
+	 */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getAddress() {
-        return address;
-    }
+	/*
+	 * public String getAddress() { return address; }
+	 */
     public void setAddress(String address) {
         this.address = address;
     }
