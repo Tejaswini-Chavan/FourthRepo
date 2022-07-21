fb62e9b6502380abad1021817461dbf15b719b0c
Tejaswini <oktapoc11@gmail.com>
2022-07-21 16:54:25 +0530

    updating method

--- XmartApp/src/main/java/com/demo/xmartapp/service/CustomerServiceImpl.java
+++ XmartApp/src/main/java/com/demo/xmartapp/service/CustomerServiceImpl.java
@@ -4,26 +4,21 @@ import com.demo.xmartapp.dto.CustomerDTO;
 import com.demo.xmartapp.entity.CustomerEntity;
 import com.demo.xmartapp.repository.CustomerRepository;
 
-
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 @Service
-public class CustomerServiceImpl implements CustomerService{
+public class CustomerServiceImpl implements CustomerService {
 
-    @Autowired
-    private CustomerRepository customerRepositoryImpl;
+	@Autowired
+	private CustomerRepository customerRepositoryImpl;
 
-    
-
-    @Override
-    public CustomerEntity registerNewCustomer(CustomerDTO customerDTO) {
+	@Override
+	public CustomerEntity registerNewCustomer(CustomerDTO customerDTO) {
 
 		/*
 		 * CustomerEntity customerEntity = new CustomerEntity();
@@ -33,17 +28,15 @@ public class CustomerServiceImpl implements CustomerService{
 		 * customerEntity.setName(customerDTO.getName());
 		 * customerEntity.setPhoneNumber(customerDTO.getPhoneNumber());
 		 */
-    	CustomerEntity customerEntity = new CustomerEntity(customerDTO);
-			return customerRepositoryImpl.save(customerEntity);
+		CustomerEntity customerEntity = new CustomerEntity(customerDTO);
+		return customerRepositoryImpl.save(customerEntity);
 
-    }
-    
-    
-    @Override
-	public List<CustomerEntity> fetchCustomerList()
-    {
-        return customerRepositoryImpl.findAll();
-    }
+	}
+
+	@Override
+	public List<CustomerEntity> fetchCustomerList() {
+		return customerRepositoryImpl.findAll();
+	}
 
 	@Override
 	public CustomerEntity updatePassword(CustomerDTO customerDTO) {
@@ -55,29 +48,29 @@ public class CustomerServiceImpl implements CustomerService{
 		 * customerEntity.setName(customerDTO.getName());
 		 * customerEntity.setPhoneNumber(customerDTO.getPhoneNumber());
 		 */
-		String id= customerDTO.getEmailId();
+		String id = customerDTO.getEmailId();
 
-		if(id !=null &&  !("".equals(id)))
-		{
-			Optional<CustomerEntity> opt = customerRepositoryImpl.findById(id);
-			CustomerEntity customerEntityBody = new CustomerEntity(customerDTO);
-			if(opt.isPresent())
-				customerEntityBody = opt.get();
-
-
-		 if (Objects.nonNull(customerEntity.getPassword()) && !"".equalsIgnoreCase(customerEntity.getPassword()))
-	    	 customerEntityBody.setPassword(customerEntity.getPassword());
-		 return customerRepositoryImpl.save(customerEntityBody);
+		if (id != null && !("".equals(id))) {
+			CustomerEntity customerEntityBody= getCustomerEntityBody(customerDTO,id);
+			
+			if (Objects.nonNull(customerEntity.getPassword()) && !"".equalsIgnoreCase(customerEntity.getPassword()))
+				customerEntityBody.setPassword(customerEntity.getPassword());
+			return customerRepositoryImpl.save(customerEntityBody);
 		}
-
 		return null;
-     }
-
+	}
 	
+	private CustomerEntity getCustomerEntityBody(CustomerDTO customerDTO, String id) {
+		Optional<CustomerEntity> opt = customerRepositoryImpl.findById(id);
+		CustomerEntity customerEntityBody = new CustomerEntity(customerDTO);
+		if (opt.isPresent())
+			customerEntityBody = opt.get();
+		return customerEntityBody;
+	}
+
 	@Override
 	public void deleteCustomerById(String emailId) {
-		customerRepositoryImpl.deleteById(emailId);		
+		customerRepositoryImpl.deleteById(emailId);
 	}
-    
-    
+
 }
